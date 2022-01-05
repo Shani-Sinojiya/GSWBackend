1f4234038745b237d3de8301e73c47120b06adec
Shani Sinojiya <72117665+Shani9880@users.noreply.github.com>
2022-01-05 15:28:16 +0530

    Delete src directory

--- src/helpers/passwordHash-helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const bcrypt = require("bcrypt");
-
-const passwordHash = async (pass) => {
-  const passw = await bcrypt.hash(pass, 10);
-  return passw;
-};
-
-module.exports = passwordHash;
--- src/helpers/passwordHashMaching-helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const bcrypt = require("bcrypt");
-
-const passwordHashMatching = async (pass, Hash) => {
-  const passw = await bcrypt.compare(pass, Hash);
-  return passw;
-};
-
-module.exports = passwordHashMatching;
